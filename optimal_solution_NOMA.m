85d6dc8a28823f6922c9e08cb66e16bcf72a8855
HajarElhassani <39070664+HajarElhassani@users.noreply.github.com>
2022-07-29 23:06:14 +0200

    Add files via upload

--- optimal_solution_NOMA.m
+++ optimal_solution_NOMA.m
@@ -1,47 +1,45 @@
-%------ Dinkelbach's algorithm -------% 
+%% This function compute the GEE according to Dinkelbach's algorithm
 
 %% input: channel gain (G), A=2^(2*Rmin), maximum power budget (Pmax), circuit power (Pc)
 %% ouput: global energy efficiency (alpha)
 
 
 function alpha = optimal_solution_NOMA(G,A,Pmax,Pmin,Pc)
-
-%---- initialization ----%
+%% initialization parameters
 epsilon=1e-7;
 alpha=1e-5;
 R_P=1;
+%% lower and upper bound of feasible p1
+lb=(A(1)-1)/G(1); %lower bound
+ub=1/prod(A(2:length(G)))*(Pmax-Pmin+lb*prod(A(2:length(G)))); % upper bound
 
-
-b=(A(1)-1)/G(1);
-c=1/prod(A(2:length(G)))*(Pmax-Pmin+b*prod(A(2:length(G))));
-
-%----- Dinkelbach's algorithm ----%
+%% Dinkelbach's algorithm 
 while(R_P>epsilon)
-    a=1/(2*log(2)*alpha*prod(A(2:length(G))))-1/G(1);
-    
-    %compute the optimal power allocation
-    p(1)=min(c,max(a,b));
-    p(2)=(A(2)-1)*(1/G(2)+p(1));
+    c=1/(2*log(2)*alpha*prod(A(2:length(G))))-1/G(1);% critical point
+    %% compute the optimal power allocation
+    theta(1)=min(ub,max(c,lb));
+    theta(2)=(A(2)-1)/G(2)+theta(1)*A(2);
     
     if length(G)>2
         for k=3:length(G)
             B=0;
             for i=2:k-1
-                B=B+(A(i)-1)/G(i)*prod(A(i+1:k-1));
+                B=B+(A(i)-1)/G(i)*prod(A(i+1:k));
             end
-            p(k)=(A(k)-1)*(1/G(k)+p(1)*prod(A(2:k-1))+B);
+            theta(k)=(A(k)-1)/G(k)+theta(1)*prod(A(2:k))+B;
         end
     end
-    
-    %compute the sum rate
-    for k=1:length(G)
-        r(k)=1/2*log2(1+G(k)*p(k)/(1+G(k)*(sum(p(1:k-1)))));
+    %% compute the rates
+    r=zeros(length(G),1);
+    r(1)=1/2*log2(1+G(1)*theta(1));
+    for k=2:length(G)
+        r(k)=1/2*log2((1+G(k)*theta(k))/(1+G(k)*theta(k-1)));
     end
-    
-    R_P=sum(r)-alpha*(sum(p)+Pc);  
-    alpha=sum(r)/(sum(p)+Pc);
+    %% compute the function sum rate vs total power tradeoff
+    R_P=sum(r)-alpha*(theta(end)+Pc);  
+    %% update alpha
+    alpha=sum(r)/(theta(end)+Pc);
 
 end
-
 
 end
--- optimal_solution_OMA.m
+++ optimal_solution_OMA.m
@@ -1,37 +1,30 @@
-function [alpha,Rsum,Ptot] = optimal_solution_OMA(G,A,Pmax,Pc)
+%% This function compute the GEE according to Dinkelbach's algorithm
 
+%% input: channel gain (G), A=2^(2*Rmin), maximum power budget (Pmax), circuit power (Pc)
+%% ouput: global energy efficiency (alpha)
+function alpha = optimal_solution_OMA(G,A,Pmax,Pc)
+
+%% initialization parameters
 epsilon=1e-7;
 alpha=1e-5;
 R_P=1;
 
-P1=(A.^length(G)-1)./G;
+%% lower and upper bound of feasible p1
+lb=(A.^length(G)-1)./G;
+ub=Pmax*ones(length(G),1);
 
-P3=Pmax*ones(length(G),1);
-
-P1_1=(A(1)^length(G)-1)/G(1);
-P2_1=(A(2)^length(G)-1)/G(2);
-
-P1_3=Pmax;
-P2_3=Pmax;
-
-while(R_P>epsilon)
-
-    P2=1/(2*log(2)*alpha)-1./G;
-    P1_2=1/(2*log(2)*alpha)-1/G(1);
-    P2_2=1/(2*log(2)*alpha)-1/G(2);
+%% Dinkelbach's algorithm
+while(R_P>epsilon)   
+    c=1/(2*log(2)*alpha)-1./G; % critical point
     
-    
-    p1=min(P1_3,max(P1_1,P1_2));
-    p2=min(P2_3,max(P2_1,P2_2));
-
+    %% compute the powers
     for k=1:length(G)
-        p(k)=min(P3(k),max(P1(k),P2(k)));
+        p(k)=min(ub(k),max(lb(k),c(k)));
     end
+    %% compute the function sum rate vs total power tradeoff
+    R_P=1/(2*length(G))*sum(log2(1+G.*p'))-alpha*(sum(p)/length(G)+Pc);
     
-    R_P=(1/(2*length(G)))*sum(log2(1+G.*p'))-alpha*(sum(p)/length(G)+Pc);
-    alpha=(1/(2*length(G)))*sum(log2(1+G.*p'))/(sum(p)/length(G)+Pc);
-    
-    Rsum=(1/(2*length(G)))*sum(log2(1+G.*p'));
-    Ptot=(sum(p)/length(G))+Pc;
+    %% update alpha
+    alpha=1/(2*length(G))*sum(log2(1+G.*p'))/(sum(p)/length(G)+Pc);
 end
 end
